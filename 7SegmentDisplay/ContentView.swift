3f0d7d9fee36d2622bd310d04137a2d42e94e0a9
Tsungyu Yu <ytyubox@gmail.com>
2020-07-30 11:45:13 +0800

    Initial Commit

---
+++ 7SegmentDisplay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  7SegmentDisplay
+//
+//  Created by 游宗諭 on 2020/7/30.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

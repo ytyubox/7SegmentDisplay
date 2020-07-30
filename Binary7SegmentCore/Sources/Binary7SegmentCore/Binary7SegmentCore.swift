3aa7d8893d5fc9b4bbdf06d72e4a77b5e61ea752
Tsungyu Yu <ytyubox@gmail.com>
2020-07-30 14:24:46 +0800

    add circuit test

--- Binary7SegmentCore/Sources/Binary7SegmentCore/Binary7SegmentCore.swift
+++ Binary7SegmentCore/Sources/Binary7SegmentCore/Binary7SegmentCore.swift
@@ -1,31 +1,31 @@
 struct Binary7SegmentCore {
 }
-struct Byte {
-    internal init() {
+public struct Byte {
+    public init() {
         self._D = false
         self._C = false
         self._B = false
         self._A = false
     }
     
-    var _D,_C,_B,_A:Bool
-    var list:[Bool] {[_D,_C,_B,_A]}
-    var value:Int {
+    public var _D,_C,_B,_A:Bool
+    public var list:[Bool] {[_D,_C,_B,_A]}
+    public var value:Int {
         var i = 0
-        i = _D ? i + 1 : i
-        i = _C ? i + 2 : i
-        i = _B ? i + 4 : i
-        i = _A ? i + 8 : i
+        i += _D ? 1 : 0
+        i += _C ? 2 : 0
+        i += _B ? 4 : 0
+        i += _A ? 8 : 0
         return i
     }
-    mutating func changeTo(_ boolList:[Bool]) {
+    mutating public func changeTo(_ boolList:[Bool]) {
         assert(boolList.count == 4)
         _D = boolList[3]
         _C = boolList[2]
         _B = boolList[1]
         _A = boolList[0]
     }
-    mutating func reset() {
+    mutating public func reset() {
         _D = false
         _C = false
         _B = false
@@ -33,47 +33,35 @@ struct Byte {
     }
 }
 
-struct ByteCirCuits {
-    internal init(byte: Byte) {
+public struct ByteCirCuits {
+    public init(byte: Byte) {
     self.byte = byte
-        
+        var andList = Array(repeating: false, count: 9)
+        var orList  = Array(repeating: false, count: 7)
         andList[00] = byte._B  .and(byte._D)
         andList[01] = byte._B.n.and(byte._D.n)
         andList[02] = byte._C  .and(byte._D)
         andList[03] = byte._C.n.and(byte._D.n)
-        andList[04] = byte._C  .and(byte._D)
+        andList[04] = byte._C  .and(byte._D.n)
         andList[05] = byte._B.n.and(byte._C)
         andList[07] = byte._B  .and(byte._C.n)
-        andList[08] = byte._B   .and(byte._D)
+        andList[08] = byte._B  .and(byte._D.n)
         andList[06] = byte._D  .and(andList[7])
         
-        orList[00] = (andList[0] || andList[1] || byte._A || byte._C)
-//        orList[01]
-//        orList[02]
-//        orList[03]
-//        orList[04]
-//        orList[05]
-//        orList[06]
-        
+        orList[00] = (andList[0] || andList[1] || byte._A     || byte._C)
+        orList[01] = (andList[2] || byte._B.n  || andList[3])
+        orList[02] = (byte._B    || byte._C.n  || byte._D)
+        orList[03] = (byte._A    || andList[1] || andList[4]  || andList[5] || andList[6])
+        orList[04] = (andList[4] || andList[1])
+        orList[05] = andList[3]  || andList[8] || andList[7]  || byte._A
+        orList[06] = andList[4]  || andList[5] || andList[7]  || byte._A    || andList[6]
+        self.andList = andList
+        self.orList = orList
     }
     
-    var andList = Array(repeating: false, count: 9)
-    var orList  = Array(repeating: false, count: 7)
-    var byte:Byte
-}
-
-struct Decimal7SegmentPresenter {
-    internal init() {
-        self.a = false
-        self.b = false
-        self.c = false
-        self.d = false
-        self.e = false
-        self.f = false
-        self.g = false
-    }
-    
-    var a,b,c,d,e,f,g :Bool
+    public let andList :Array<Bool>
+    public let orList  :Array<Bool>
+    public let byte:Byte
 }
 
 extension Bool {
--- Binary7SegmentCore/Tests/Binary7SegmentCoreTests/Binary7SegmentCoreTests.swift
+++ Binary7SegmentCore/Tests/Binary7SegmentCoreTests/Binary7SegmentCoreTests.swift
@@ -1,32 +1,76 @@
 import XCTest
-@testable import Binary7SegmentCore
+import Binary7SegmentCore
 
 final class Binary7SegmentCoreTests: XCTestCase {
-    fileprivate func testValue(_ list: [Int], _ expect: Int) {
+    fileprivate func testValue(_ list: Int, _ expect: Int) {
         var byte = Byte()
-        byte.changeTo(list.booled())
+        byte.changeTo(list.boolList(count: 4))
         XCTAssertEqual(byte.value, expect,"\(list) expect \(expect), but found \(byte.value)")
-        byte.reset()
     }
     
     func testGetValue() {
         let testList = [
-            ([0,0,0,0], 0),
-            ([0,0,0,1], 1),
-            ([0,0,1,0], 2),
-            ([0,0,1,1], 3),
-            ([0,1,0,0], 4),
-            ([0,1,0,1], 5),
-            ([0,1,1,0], 6),
-            ([0,1,1,1], 7),
-            ([1,0,0,0], 8),
-            ([1,0,0,1], 9),
+            (0000, 0),
+            (0001, 1),
+            (0010, 2),
+            (0011, 3),
+            (0100, 4),
+            (0101, 5),
+            (0110, 6),
+            (0111, 7),
+            (1000, 8),
+            (1001, 9),
         ]
+        
         for (list, expect) in testList {
         testValue(list, expect)
         }
     }
-
+    func testOrList() {
+        let testList = [
+            (0000, 010100000, 1111110),
+            (0001, 000000000, 0110000),
+            (0010, 010011000, 1101101),
+            (0011, 001001000, 1111001),
+            (0100, 000100011, 0110011),
+            (0101, 100000110, 1011011),
+            (0110, 000010001, 1011111),
+            (0111, 101000000, 1110000),
+            (1000, 010100000, 1111111),
+            (1001, 000000000, 1111011),
+        ]
+        
+        for (list, expect1, expect2) in testList {
+            var byte = Byte()
+            byte.changeTo(list.boolList(count: 4))
+            let byteCircuits = ByteCirCuits(byte: byte)
+            XCTAssertEqual(byteCircuits.andList.count, 9)
+            XCTAssertEqual(byteCircuits.orList.count, 7)
+            XCTAssertEqual(
+                expect1.boolList(count: 9), byteCircuits.andList
+            )
+            XCTAssertEqual(expect2.boolList(count: 7), byteCircuits.orList, "\(list) expected \(expect2.boolList(count: 7)) but found \(byteCircuits.orList)")
+        }
+        
+    }
+    func testIntToBoolList() {
+        let list = [ (0000,[0,0,0,0]),
+                     (0001,[0,0,0,1]),
+                     (0010,[0,0,1,0]),
+                     (0011,[0,0,1,1]),
+                     (0100,[0,1,0,0]),
+                     (0101,[0,1,0,1]),
+                     (0110,[0,1,1,0]),
+                     (0111,[0,1,1,1]),
+                     (1000,[1,0,0,0]),
+                     (1001,[1,0,0,1])]
+        for i in list {
+            let result = i.0.boolList(count: 4)
+            let expect = i.1
+            XCTAssertEqual(result, expect.booled())
+        }
+        
+    }
     static var allTests = [
         ("testGetValue", testGetValue),
     ]
@@ -35,5 +79,20 @@ final class Binary7SegmentCoreTests: XCTestCase {
 extension Array where Element == Int {
     func booled() -> [Bool] {
         map{$0 != 0}
+    }
+}
+
+extension Int {
+    func boolList(count:Int) -> [Bool] {
+        var copy = self
+        var re = [Bool]()
+        while copy > 0 {
+            re.append(copy % 10 == 0 ? false : true)
+            copy /= 10
+        }
+        while re.count < count {
+            re.append(false)
+        }
+        return re.reversed()
     }
 }
